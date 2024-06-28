e2cab0129ecdd5e39c617ca871943b8b3fd43751
Devopswithaws58 <devopswithaws58@gmail.com>
2024-06-29 01:48:31 +0530

    jenkins and eks bucket names are modified with same name

--- jenkins/backend.tf
+++ jenkins/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-eks-cicd-9176"
+    bucket = "terraform-eks-cicd-9399"
     key    = "jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
