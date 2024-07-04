0864276bd2953daf68305eaaa21f529940bf4535
Devopswithaws58 <devopswithaws58@gmail.com>
2024-07-04 19:16:54 +0530

    modified main.tf file

--- EKS/main.tf
+++ EKS/main.tf
@@ -14,7 +14,6 @@ module "vpc" {
   enable_nat_gateway      = true
   single_nat_gateway      = true
   enable_dns_hostnames    = true
-  enable_dns_support      = true
 
   public_subnet_tags = {
     "Name"                                 = "eks-jenkins-public-subnet"
