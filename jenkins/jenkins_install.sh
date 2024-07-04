18649d04af2f3d58958ce8aae2291c9646060e48
Devopswithaws58 <devopswithaws58@gmail.com>
2024-07-04 18:13:00 +0530

    modified file

--- jenkins/jenkins_install.sh
+++ jenkins/jenkins_install.sh
@@ -26,7 +26,7 @@ sudo yum install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
-# install Kubernetes
+# install Kubectl
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
