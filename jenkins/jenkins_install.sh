901ac703e506981f83225f9f35ea518250ae17f3
Devopswithaws58 <devopswithaws58@gmail.com>
2024-07-04 17:30:39 +0530

    modified yaml and jenkins file

--- jenkins/jenkins_install.sh
+++ jenkins/jenkins_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum update -y && sudo yum install wget -y
 
+# install jdk and jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
@@ -11,17 +12,21 @@ sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
+# install git
 sudo yum install git -y
 
+# install terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
+#install aws cli
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo yum install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
+# install Kubernetes
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
